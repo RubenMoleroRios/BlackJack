8f50ab3465a7110052c1030cd13e18e78a3ab3b5
Francisco Rubén Molero Rios <rubenmolero@outlook.es>
2025-05-27 10:09:03 +0200

    Function orderCard ready

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,6 +10,8 @@ let deck = [];
 const types = ['C','D','H','S'];
 const specials = ['A','J','Q','K'];
 
+
+//función para crear una baraja nueva
 const createDeck = () =>{
     for ( let i = 2; i <= 10; i++){
         for(let type of types){
@@ -23,10 +25,25 @@ const createDeck = () =>{
         }
     }
 
-    console.log(deck);
+    //console.log(deck);
     deck = _.shuffle(deck);
     console.log(deck);
     return deck;
 }
 
 createDeck();
+
+//Esta funcion permite pedir una carta
+
+const orderCard = () => {
+    
+    if(deck.length===0){
+        throw 'No hay cartas en el deck';
+    }
+    const card = deck.pop();
+    console.log(deck);
+    //console.log(card);
+    return '2C';
+}
+
+orderCard();
